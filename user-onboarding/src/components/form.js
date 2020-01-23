b385207fd76f806f2bb9d320a5396476fd38bf5e
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-22 19:44:16 -0700

    Setup <Form> markup in Form.js (Step 1 in the README.md)

--- user-onboarding/src/components/form.js
+++ user-onboarding/src/components/form.js
@@ -3,13 +3,62 @@ import { Form, Field, withFomrik } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
-const Form = ({ errors, touched, values, status }) => {
+const NewUserForm = ({ errors, touched, values, status }) => {
     const [userInfo, setUserInfo] = useState ([]);
 
     useEffect (() => {
         status && setUserInfo (userInfo => [...userInfo, status]);
     }, [status])
 
+    return (
+        <div className="new-user-form">
+            <Form>
+                <h1>New User Form</h1>
+
+                <Field 
+                    type="text"
+                    name="name"
+                    placeholer="Name"
+                    value={values.name}
+                />
+                {touched.name && errors.name && <p>{errors.name}</p>}
+
+                <Field 
+                    type="text"
+                    name="email"
+                    placeholer="E-mail"
+                    value={values.email}
+                />
+                {touched.email && errors.email && <p>{errors.email}</p>}
+
+                <Field 
+                    type="text"
+                    name="password"
+                    placeholer="Password"
+                    value={values.password}
+                />
+                {touched.password && errors.password && <p>{errors.password}</p>}
+
+                <label>
+                <Field 
+                    type="checkbox"
+                    name="terms-of-service"
+                    value={values.checkbox}
+                />
+                </label>
+
+                <button type="submit">Submit</button>
+
+            </Form>
+        </div>
+    )
+
 }
 
-export default Form;
+export default NewUserForm;
+
+// Name
+//  Email
+//  Password
+//  Terms of Service (checkbox)
+//  A Submit button to send our form data to the server.
