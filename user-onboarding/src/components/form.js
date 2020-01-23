6333b20dcb11eb41669265a98623e36a11b52837
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-22 19:30:06 -0700

    imported useState, useEffect, Fomr Field, withFormik, * as Yup, and axios into Form.js. Started my const Form in Form.js and set up my useState for userInfo.

--- user-onboarding/src/components/form.js
+++ user-onboarding/src/components/form.js
@@ -1,1 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
+import { Form, Field, withFomrik } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+const Form = ({ errors, touched, values, status }) => {
+    const [userInfo, setUserInfo] = useState ([]);
+
+}
