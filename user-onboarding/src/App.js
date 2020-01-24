67fefbd5619c7b1366c81dba9526319fd7823e62
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-23 17:44:00 -0700

    Changed the background image

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -7,9 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="form">
-          <Form />
-        </div>
+        <Form />
       </header>
     </div>
   );
